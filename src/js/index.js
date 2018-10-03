c2033a0fd32f2ee0bc7af986a11aebcde364f7ff
vincent <vincent91500@gmail.com>
2018-10-03 21:24:57 +0800

    解决弹幕不能覆盖视频的问题，添加视频控制
    添加视频控制，解决弹幕不能覆盖视频的问题。
    目前bug:
    1.音量不能控制
    2.直播标题覆盖视频，目前被注释

--- src/js/index.js
+++ src/js/index.js
@@ -23,35 +23,49 @@ $(document).ready(function () {
     }
     //设置播放器大小
     cons("窗口高度:" + $(window).height() + " header高度:" + $("header").height() + " 控制条高度:" + $(".controls").height() + " title高度:" + $(".title-section").height());
-    $("video").css("max-height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
-    $("#player").css("height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
-    // $("video").css("width", $(window).width() * 0.9 + "px");
-    //----控制模块---
-    // if (isiOS) { //如果是iOS则不显示控制条
-    //     $(".controls").css("display", "none");
-    // }
-    // var movie = document.getElementById("video");
-    // movie.play();
-    // playing = true;
-    // $("#play").css("background-image", "url(src/img/pause.SVG)");
-    // $("#vol").val(movie.volume * 100);
-    // $("#play").click(function () {
-    //     if (playing) {
-    //         movie.pause();
-    //         playing = false;
-    //         $("#play").css("background-image", "url(src/img/play.SVG)");
-    //     } else {
-    //         movie.play();
-    //         playing = true;
-    //         $("#play").css("background-image", "url(src/img/pause.SVG)");
-    //     }
-    // });
-    // $("#vol").change(function () {
-    //     movie.volume = $("#vol").val() / 100;
-    // });
+    //$("video").css("max-height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
+    //$("#player").css("height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
+	$("video").width($(window).width()*0.6);
+	$("#player").width($(window).width()*0.6);
+	$('#danmu').css("max-width",($("video").width()));
+	$('#danmu').css("max-height",($("video").height()));
+    $('#danmu').width($("video").width);
+	$('#danmu').height($("video").height);
+    $('.videoframe').css("max-width",($("video").width()));
+	$('.videoframe').css("max-height",($("video").height()));
+    $('.videoframe').width($(window).width);
+	$('.videoframe').height($(window).height);
+    $('video').trigger('play');
+    playing = true;
+    $("#play").css("background-image", "url(src/img/pause.SVG)");
+    $("#vol").val($("video").volume * 100);
+    $("#vol").change(function () {
+         $("video").volume = $("#vol").val() / 100;
+     });
 });
 $(window).resize(function () {  //当浏览器大小变化时
     cons("窗口高度:" + $(window).height() + " header高度:" + $("header").height() + " 控制条高度:" + $(".controls").height() + " title高度:" + $(".title-section").height());
-    $("video").css("max-height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
-    $("#player").css("height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
+    //$("video").css("max-height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
+    //$("#player").css("height", ($(window).height() - $("header").height() - $(".title-section").height()) * 0.8 + "px");
+	$("video").width($(window).width()*0.6);
+	$("#player").width($(window).width()*0.6);
+	$('#danmu').css("max-width",($("video").width()));
+	$('#danmu').css("max-height",($("video").height()));
+    $('#danmu').width($("video").width);
+	$('#danmu').height($("video").height);
+    $('.videoframe').css("max-width",($("video").width()));
+	$('.videoframe').css("max-height",($("video").height()));
+    $('.videoframe').width($(window).width);
+	$('.videoframe').height($(window).height);
 });
+function play(){
+if (playing) {
+             $('video').trigger('pause');
+             playing = false;
+             $("#play").css("background-image", "url(src/img/play.SVG)");
+         } else {
+             $('video').trigger('play');
+             playing = true;
+             $("#play").css("background-image", "url(src/img/pause.SVG)");
+         }
+}
